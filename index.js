7cb635ecfeb59bb07abc05b1a3144fd2882ca0a9
bertW87 <77349387+bertW87@users.noreply.github.com>
2021-07-16 02:54:38 -0700

    initial

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const app = express();
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/services', (req, res) => res.render('pages/services'))
+  .get('/termsofuse', (req, res) => res.render('pages/termsofuse'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
